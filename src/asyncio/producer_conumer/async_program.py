ad76619533eb6c5e5a5f6f17554c3bf511d30774
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-09 12:17:19 -0800

    async demos & start of web scraping example

--- src/asyncio/producer_conumer/async_program.py
+++ src/asyncio/producer_conumer/async_program.py
@@ -6,11 +6,9 @@ import colorama
 
 
 def main():
-
     loop = asyncio.get_event_loop()
-
     t0 = datetime.datetime.now()
-    print(colorama.Fore.WHITE + "App started.", flush=True)
+    print(f"{colorama.Fore.WHITE } App started.", flush=True)
 
     # data structure- first in first out
     data = asyncio.Queue()
@@ -21,9 +19,7 @@ def main():
     task3 = loop.create_task(process_data(40, data))
 
     final_task = asyncio.gather(task1, task2, task3)
-
     loop.run_until_complete(final_task)
-
     dt = datetime.datetime.now() - t0
 
     print(
@@ -34,15 +30,14 @@ def main():
 
 
 async def generate_data(num: int, data: asyncio.Queue):
+    # breaking up functions to wait while we do something
     for idx in range(1, num + 1):
         item = idx * idx
-
         # run until `item` then wait here...
         await data.put((item, datetime.datetime.now()))
 
         # ... then do this
-        print(colorama.Fore.YELLOW + f" -- generated item {idx}", flush=True)
-
+        print(f"{colorama.Fore.YELLOW} -- generated item {idx}", flush=True)
         # then wait until done sleeping then start loop over
         await asyncio.sleep(random.random() + 0.5)
 
@@ -58,8 +53,7 @@ async def process_data(num: int, data: asyncio.Queue):
         dt = datetime.datetime.now() - t
 
         print(
-            colorama.Fore.CYAN
-            + f" +++ Processed value {value} after {dt.total_seconds():,.2f} sec.",
+            f"{colorama.Fore.CYAN} +++ Processed value {value} after {dt.total_seconds():,.2f} sec.",
             flush=True,
         )
         await asyncio.sleep(0.5)
