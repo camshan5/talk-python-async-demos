5c06e7f5b86a95183c4d463e484f236061c3c875
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-09 14:56:40 -0800

    Completed async scraper

--- src/asyncio/web_scraping/async_scrape.py
+++ src/asyncio/web_scraping/async_scrape.py
@@ -1,24 +1,34 @@
-import requests
+import asyncio
+
+import aiohttp
 import bs4
 from colorama import Fore
 
 
-def get_html(episode_number: int) -> str:
+# Older versions of python require calling loop.create_task() rather than on asyncio.
+# Make this available more easily.
+global loop
+
+
+async def get_html(episode_number: int) -> str:
     print(Fore.YELLOW + f"Getting HTML for episode {episode_number}", flush=True)
 
-    url = f'https://talkpython.fm/{episode_number}'
+    url = f"https://talkpython.fm/{episode_number}"
 
     # waiting at this response before async
-    resp = requests.get(url)
-    resp.raise_for_status()
+    # resp = requests.get(url)
 
-    return resp.text
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            resp.raise_for_status()
+
+            return await resp.text()
 
 
 def get_title(html: str, episode_number: int) -> str:
     print(Fore.CYAN + f"Getting TITLE for episode {episode_number}", flush=True)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    header = soup.select_one('h1')
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    header = soup.select_one("h1")
     if not header:
         return "MISSING"
 
@@ -26,17 +36,33 @@ def get_title(html: str, episode_number: int) -> str:
 
 
 def main():
-    get_title_range()
+    global loop
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_title_range())
     print("Done.")
 
 
-def get_title_range():
+async def get_title_range_old_version():
     # Please keep this range pretty small to not DDoS my site. ;)
     for n in range(150, 160):
-        html = get_html(n)
+        html = await get_html(n)
         title = get_title(html, n)
         print(Fore.WHITE + f"Title found: {title}", flush=True)
 
 
-if __name__ == '__main__':
+async def get_title_range():
+    # What we need to do for performance benefits is to start all the
+    # requests then go process the responses as they come in...
+
+    tasks = []
+    for n in range(150, 160):
+        tasks.append((n, loop.create_task(get_html(n))))
+
+    for n, t in tasks:
+        html = await t
+        title = get_title(html, n)
+        print(Fore.WHITE + f"Title found: {title}", flush=True)
+
+
+if __name__ == "__main__":
     main()
